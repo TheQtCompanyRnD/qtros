b92ca0194f28fe540534be8292059728792ac90d
Christian Stromme <christian.stromme@qt.io>
2026-05-21 17:38:46 +0200

    Add license files under the tools directory

--- tools/urdfviewer/src/ImporterWindow.cpp
+++ tools/urdfviewer/src/ImporterWindow.cpp
@@ -1,3 +1,5 @@
+// Copyright (C) 2026 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 #include "ImporterWindow.h"
 
 #include "importer_config.h"
